8f11eedcf99758d91cf5856337d76a3270a0138a
Judith <judith.barrajon@gmail.com>
2022-09-06 15:53:29 +0200

    añadida seguridad

--- src/main/java/com/gft/moviesapi/security/ApiSecurity.java
+++ src/main/java/com/gft/moviesapi/security/ApiSecurity.java
@@ -4,43 +4,46 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ApiSecurity extends WebSecurityConfigurerAdapter {
-        @Autowired
-        private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-        @Autowired
-        public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-            auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                    .dataSource(dataSource)
-                    .usersByUsernameQuery("select username, password, enabled from users where username=?")
-                    .authoritiesByUsernameQuery("select username, role from users where username=?");
-
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests()
-                    .antMatchers("/h2-console/**").permitAll()
-                    .and().csrf().ignoringAntMatchers("/h2-console/**")
-                    .and().headers().frameOptions().sameOrigin()
-                    .and().authorizeRequests()
-                    .anyRequest()
-                    .authenticated()
-                    .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .csrf().disable()
-                    .httpBasic();
-        }
-
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?");
 
     }
+
+    /*http://localhost:8080/h2-console*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin()
+                .and().authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .httpBasic();
+    }
+
 }
 
 
